d507c455695ebe45c3de39ccf0d957da84b847f8
alexismayfire <calexandrelvieira@gmail.com>
2018-07-04 20:33:15 -0300

    Ajustes de thread e controller

--- Web/Lib/GeraBitmap.cs
+++ Web/Lib/GeraBitmap.cs
@@ -6,15 +6,21 @@ namespace Web.Lib
 {
     public class GeraBitmap
     {
-        public void ToBitmap(double[] rawImage, string nomeTxt)
+        public void ToBitmap(double[] rawImage, string nomeTxt, string path)
         {
             double min = 5000000, max = 0, den;
             int width = 60;
             int height = 60;
-            var path = HttpContext.Current.Server.MapPath("~/Content/Images/");
-            DateTime a = DateTime.Now;
+            //var path = HttpContext.Current.Server.MapPath("~/Content/Images/");
+            DateTime dataHora = DateTime.Now;
+            String dataHoraString = dataHora.Day.ToString() + "."
+                                    + dataHora.Month.ToString() + "."
+                                    + dataHora.Year.ToString() + "-"
+                                    + dataHora.Hour.ToString() + "."
+                                    + dataHora.Minute.ToString() + "."
+                                    + dataHora.Second.ToString();
             string nomeBmp = path + nomeTxt.Replace(".txt", "");
-            nomeBmp = nomeBmp + "%" + a.ToString() + "%" + ".bmp";
+            nomeBmp = nomeBmp + "%" + dataHoraString + "%" + ".bmp";
             Bitmap Image = new Bitmap(width, height);
 
             for (int i = 0; i < width * height; i++)
